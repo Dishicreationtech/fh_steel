e93f1ea5c10180c952c71d5829458a5edc9ba9e6
shonpuri <shonpuri@gmail.com>
2023-04-26 11:30:19 +0530

    Update on controller for dealer

--- controllers/main.py
+++ controllers/main.py
@@ -18,8 +18,8 @@ class Websession(http.Controller):
             dealers_ids = request.env['res.partner'].sudo().search([("user_id", "=", user.id),("add_lead", "=", "dealer")])
             # ,("add_lead", "=", "dealer")#######
             data.update({
-                'contacts': [{'id':contact.id, 'name': contact.name, 'mobile':  contact.mobile, 'email' : contact.email, 'user_id':user.id} for contact in user_ids]
-                'dealer': [{'id':contact.id, 'name': contact.name, 'mobile':  contact.mobile, 'email' : contact.email, 'user_id':user.id} for contact in dealers_ids]                
+                'contacts': [{'id':contact.id, 'name': contact.name, 'mobile':  contact.mobile, 'email' : contact.email, 'user_id':user.id} for contact in dealers_ids]
+                              
                 })
             return data
         else:
