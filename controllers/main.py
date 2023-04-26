dd44bc09ae1b05826339d4d62fc5a4cd3478e53c
shonpuri <shonpuri@gmail.com>
2023-04-26 23:34:39 +0530

    Update on controller for Task API Final

--- controllers/main.py
+++ controllers/main.py
@@ -177,6 +177,6 @@ class Websession(http.Controller):
     @http.route('/get_task_information', type='json', auth='public', cors="*" ,csrf=False)
     def get_task_information(self, user_id):
         line_ids = []
-        task_ids = request.env['mail.activity'].sudo().search(['user_id','=', data.get('user_id')])
+        task_ids = request.env['mail.activity'].sudo().search(['user_id','=', user_id ])
         line_ids = [{'task_id' : record.id, 'task_date': record.date_deadline, 'task_user_id' : record.user_id , 'task_summary' : record.summary ,'task_note' : record.note } for record in task_ids]
         return {'status_message' : 200, 'task_info' : line_ids}
